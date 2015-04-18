e59d3d2356589cce3e2c577328c72b32f20808ae
Keshuai Xu <i@xks.im>
2015-04-18 18:37:40 -0400

    fixed memory leak

--- MiniMotorController.ino
+++ MiniMotorController.ino
@@ -2,7 +2,7 @@
 #define ENCODER_OPTIMIZE_INTERRUPTS
 #include <PWM.h>
 #include <Wire.h>
-
+#include <MemoryFree.h>
 #include <Encoder.h>
 #include <RegulatedMotor.h>;
 #include <KinematicController.h>
@@ -11,15 +11,19 @@
 Encoder enc1(2,4);
 Encoder enc2(3,5);
 
-RegulatedMotor m1(&enc1,7,6,10);
-RegulatedMotor m2(&enc2,8,11,9);
+long encoder1;
+long encoder2;
+
+RegulatedMotor m1(&encoder1,7,6,10);
+RegulatedMotor m2(&encoder2,8,11,9);
 
 KinematicController kc(&m1,&m2,1,1,100,50,64*4*30);
 
 void setup(){
 	Serial.begin(115200);
-	Wire.begin(0x01);
+	Wire.begin(MOTOR_CONTROLLER_ADDRESS);
 	Wire.onReceive(receiveEvent);
+	Wire.onRequest(requestEvent);
 	pinMode(6,OUTPUT);
 	pinMode(7,OUTPUT);
 	pinMode(8,OUTPUT);
@@ -42,12 +46,15 @@ void setup(){
 long lastHeartbeat = 0;
 
 void loop(){
+	encoder1 = enc1.read();
+	encoder2 = enc2.read();
 	kc.run();
 	m1.run();
 	m2.run();
 
 	if ( millis()-lastHeartbeat > 5000 ){
 	}
+
 }
 
 
@@ -58,21 +65,33 @@ void receiveEvent(int howMany){
 	uint16_t arg[16];
 	switch (command) {
 		case COMMAND_HEARTBEAT:
-		lastHeartbeat = millis();
+			lastHeartbeat = millis();
 		break;
 		case COMMAND_SETACCELERATION:
-		for (int i = 0; i < 4; i++){
-			arg[i] = Wire.read();
-			arg[i] |= Wire.read() << 8;
-		}
-		kc.setAcceleration(arg[0],arg[1],arg[2],arg[3]);
+			for (int i = 0; i < 4; i++){
+				arg[i] = Wire.read();
+				arg[i] |= Wire.read() << 8;
+			}
+			kc.setAcceleration(arg[0],arg[1],arg[2],arg[3]);
 		break;
 		case COMMAND_GOVELOCITY:
-		for (int i = 0; i < 2; i++){
-			arg[i] = Wire.read();
-			arg[i] |= Wire.read() << 8;
-		}
-		kc.goVelocity(arg[0],arg[1]);
+			for (int i = 0; i < 2; i++){
+				arg[i] = Wire.read();
+				arg[i] |= Wire.read() << 8;
+			}
+			kc.goVelocity(arg[0],arg[1]);
+		break;
+		case COMMAND_BRAKE:
+			kc.brake();
+		break;
+		case COMMAND_COAST:
+			kc.coast();
+		break;
+		case COMMAND_REPORTSTANDBY:
+			responseState = COMMAND_REPORTSTANDBY;
+		break;
+		case COMMAND_REPORTENCODER:
+			responseState = COMMAND_REPORTENCODER;
 		break;
 		default:
 		break;
@@ -81,4 +100,26 @@ void receiveEvent(int howMany){
 		Wire.read();
 		Serial.println("too many");
 	}
+}
+
+void requestEvent(){
+	if (responseState == COMMAND_REPORTSTANDBY) {
+		byte resp = kc.isStandby()?0x01:0x00;
+		Wire.write(resp);
+		responseState = 0x00;
+	} else if (responseState == COMMAND_REPORTENCODER){
+		byte resp[8];
+		int32_t fwd = kc.getOdometryForward();
+		int32_t ccw = kc.getOdometryCCW();
+		resp[0] = fwd;
+		resp[1] = fwd >> 8;
+		resp[2] = fwd >> 16;
+		resp[3] = fwd >> 24;
+		resp[4] = ccw;
+		resp[5] = ccw >> 8;
+		resp[6] = ccw >> 16;
+		resp[7] = ccw >> 24;
+		Wire.write(resp,8);
+	}
+
 }
